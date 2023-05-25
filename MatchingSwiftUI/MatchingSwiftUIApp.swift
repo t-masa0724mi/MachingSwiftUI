528378b756b21264842c3c82248de8eb8e604c05
masa950724mi <s100.iizuka.masami@gmail.com>
2023-05-25 12:31:52 +0900

    Initial Commit

---
+++ MatchingSwiftUI/MatchingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingSwiftUIApp.swift
+//  MatchingSwiftUI
+//
+//  Created by 飯塚政美 on 2023/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MatchingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
